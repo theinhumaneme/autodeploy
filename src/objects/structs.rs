a63b0a3bf57c75350d1e8f9b10345260018a168f
Kalyan Mudumby <71078441+theinhumaneme@users.noreply.github.com>
2024-08-05 22:24:59 +0530

    config: make config more readable
    leverage serde to make config more readable
    
    add context

--- src/objects/structs.rs
+++ src/objects/structs.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct GlobalConfiguration {
+    #[serde(rename(deserialize = "banner"))]
     pub print_banner: bool,
     pub client: Option<String>,
     pub organization: Option<String>,
@@ -10,24 +11,35 @@ pub struct GlobalConfiguration {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct ProjectConfiguation {
+    #[serde(rename(deserialize = "path"))]
     pub repository_path: String,
     pub service: Vec<Service>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Service {
+    #[serde(rename(deserialize = "application_name"))]
     pub name: String,
-    #[serde(rename = "repository_url")]
+    #[serde(rename(deserialize = "url"))]
     pub repository_url: String,
+    #[serde(rename(deserialize = "directory_name"))]
     pub slug: String,
     pub container: Container,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Container {
+    #[serde(rename(serialize = "container_name", deserialize = "name"))]
     pub name: String,
     pub image: String,
+    #[serde(rename(deserialize = "restart_policy"))]
     pub restart: String,
+    #[serde(rename(deserialize = "docker_user_group_id"))]
+    pub user: Option<String>,
+    #[serde(rename(deserialize = "standard_in"))]
+    pub std_in: Option<bool>,
+    #[serde(rename(deserialize = "interactive"))]
+    pub tty: Option<bool>,
     pub ports: Vec<Vec<String>>,
     pub volumes: Vec<Vec<String>>,
     pub build: Build,
@@ -35,5 +47,6 @@ pub struct Container {
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Build {
+    pub context: String,
     pub dockerfile: String,
 }
